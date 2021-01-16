e843bc3360cdd82cfb82a347ef5c0cdb4b89115f
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2021-01-16 15:46:07 -0500

    se actualiza footer y se organiza navbar

---
+++ hojaVida.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Competencias</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Short+Stack&display=swap" rel="stylesheet">
+</head>
+
+<body class="bg-secondary">
+    <header>
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    <a class="navbar-brand" href=""> <i class="bi bi-code-slash p-3"></i><strong>Portafolio Web Carlos
+                            Diaz</strong></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-outline-primary rounded-pill p-3" href="index.php"><i class="bi bi-pie-chart-fill"></i><strong> Competencias</strong></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-outline-primary rounded-pill p-3" href="proyectos.php"><i class="bi bi-file-code-fill"></i><strong> Proyectos elaborados</strong></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active btn btn-outline-primary rounded-pill p-3" href="hojaVida.php"><i class="bi bi-arrow-down-square-fill"></i> <strong>Hoja de vida</strong></a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col mt-5">
+                    <h1 class="text-center mt-3"><i class="bi bi-lightning-fill"></i><strong>Mi portafolio web</strong></h1>
+                    <div class="alert alert-secondary" role="alert">
+                        <div class="row mb-2">
+                            <div class="col">
+                                <h2><strong>Herramientas utilizadas</strong></h2>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                                    <ol class="carousel-indicators">
+                                        <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+                                        <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+                                        <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+                                    </ol>
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="https://placeimg.com/1000/480/animals" class="d-block w-100 rounded" alt="...">
+                                            <div class="carousel-caption d-none d-md-block">
+                                                <h5 class="text-dark">First slide label</h5>
+                                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="https://placeimg.com/1000/480/nature" class="d-block w-100 rounded" alt="...">
+                                            <div class="carousel-caption d-none d-md-block">
+                                                <h5>Second slide label</h5>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="https://placeimg.com/1000/480/tech" class="d-block w-100 rounded" alt="...">
+                                            <div class="carousel-caption d-none d-md-block">
+                                                <h5>Third slide label</h5>
+                                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mt-3 mb-5">
+                    <div class="alert alert-secondary border border-primary border-1" role="alert">
+                        <strong>Javascript</strong>
+                        <div class="progress border border-success border-1 col-md-8">
+                            <div class="text-light progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 75%"><strong>75%</strong></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="bg-dark text-center text-white mt-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card mb-5 mt-5 bg-secondary rounded-pill">
+                        <div class="card-body">
+                            <div class="row justify-content-center">
+                                <div class="col-md-3 mt-5 text-warning">
+                                    <p><i class="bi bi-emoji-sunglasses-fill"></i> <strong>Carlos Andres Diaz Aguirre
+                                            &copy;</strong></p>
+                                    <p><i class="bi bi-envelope-fill"></i> <strong>andrez1915@gmail.com</strong></p>
+                                    <p><i class="bi bi-geo-alt-fill"></i> <strong>Medellin - Colombia</strong></p>
+                                    <p><i class="bi bi-calendar3"></i> <strong>20<?= Date("y") ?></strong></p>
+                                </div>
+                                <div class="col-md-3 mt-3 text-warning">
+                                    <img src="https://github.com/carlitosSoporte/servidorArchivos/blob/main/creador.jpeg?raw=true" alt="creador" class="w-100 img-fluid rounded-pill mt-2 mb-3">
+                                </div>
+                                <div class="col-md-3 mt-5 text-warning">
+                                    <div class="row mb-3">
+                                        <div class="col">
+                                            <a name="" id="" class="btn btn-outline-warning rounded-circle" href="" role="button"><i class="bi bi-linkedin"></i><strong>
+                                                    Linkedin</strong></a>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col">
+                                            <a name="" id="" class="btn btn-outline-warning rounded-circle" href="" role="button"><i class="bi bi-twitter"></i><strong> Twitter</strong></a>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col">
+                                            <a name="" id="" class="btn btn-outline-warning rounded-circle" href="" role="button"><i class="bi bi-facebook"></i><strong>
+                                                    Facebook</strong></a>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <a name="" id="" target="_blank" class="btn btn-outline-warning rounded-circle" href="https://github.com/carlitosSoporte" role="button"><i class="bi bi-github"></i></i><strong> Github</strong></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</body>
+
+</html>
